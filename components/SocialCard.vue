17c51deaa2945bf07cf0c02b1b33b202e2b1125d
StasHolmansky <holmansky5@gmail.com>
2023-06-12 22:18:22 +0700

    add components

---
+++ components/SocialCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card shaped elevationt="24">
+    <v-card-text>
+      <v-row align="center">
+        Join other industry-leading organizations pushing boundaries and solving
+        problems in Figma
+        <v-spacer />
+        <v-img
+          width="44"
+          src="https://cdn.sanity.io/images/599r6htc/localized/5cc4ec7efbf529d23d98eb5cea426716848fbd56-77x77.svg?q=75&fit=max&auto=format"
+        />
+        <v-img
+          width="44"
+          src="https://cdn.sanity.io/images/599r6htc/localized/5cc4ec7efbf529d23d98eb5cea426716848fbd56-77x77.svg?q=75&fit=max&auto=format"
+        />
+        <v-img
+          width="44"
+          src="https://cdn.sanity.io/images/599r6htc/localized/5cc4ec7efbf529d23d98eb5cea426716848fbd56-77x77.svg?q=75&fit=max&auto=format"
+        />
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
